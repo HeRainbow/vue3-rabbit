27def2823b0245fe183c99f9dc5d776e09504e1e
云博 贺 <2361660078@qq.com>
2024-12-15 14:02:56 +0800

    二级组件编写和静态资源的创建

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>我是首页
+    <!-- 二级路由出口 -->
+    <RouterView/>
+  </div>
+</template>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>我是登录页</div>
+</template>
